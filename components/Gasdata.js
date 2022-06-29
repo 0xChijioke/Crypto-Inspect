b9f49560420bf807f918b6ce096f2c5ab3144baa
NaimBijapure <naim@vinnovatetechnologies.com>
2022-06-29 16:25:59 +0530

    Title: hotfix : adjust gas guzzler logic and ui

--- components/Gasdata.js
+++ components/Gasdata.js
@@ -1,103 +1,118 @@
-
+import moment from "moment";
 import { useEffect, useState } from "react";
 
 export default function Gasdata(props) {
-    let valuereceivedmainnet = 0;
-    let gasspentmainnet = 0;
-    let valuereceivedpolygon = 0;
-    let gasspentpolygon = 0;
-    let valuereceivedoptimism = 0;
-    let gasspentoptimism = 0;
+  let valuereceivedmainnet = 0;
+  let gasspentmainnet = 0;
+  let valuereceivedpolygon = 0;
+  let gasspentpolygon = 0;
+  let valuereceivedoptimism = 0;
+  let gasspentoptimism = 0;
 
-    console.log("range", props.range);
-    // console.log("inclue", String(props.range[0]).include("Jun"));
+  //   console.log("range", props.range);
+  let isDateRangeSelected = props.range.includes(false) === false;
+  let date1 = moment(props.range[0]);
+  let date2 = moment(props.range[1]);
 
+  props.mainnetdata.data.items.map((item, i) => {
+    let isInDateRange = moment(item.block_signed_at).isBetween(
+      moment(date1).format("YYYY-MM-DD"),
+      moment(date2).format("YYYY-MM-DD")
+    );
 
+    if (item.from_address != props.address.toLowerCase()) {
+      if (isDateRangeSelected && isInDateRange) {
+        valuereceivedmainnet += item.value / 10 ** 18;
+      }
 
-    props.mainnetdata.data.items.map((item, i) => {
-        console.log("date", (item.block_signed_at).charAt(8));
-        console.log("date", (item.block_signed_at).charAt(9));
-        console.log("date", (item.block_signed_at).charAt(5));
-        console.log("date", (item.block_signed_at).charAt(6));
+      if (isDateRangeSelected === false) {
+        valuereceivedmainnet += item.value / 10 ** 18;
+      }
+    }
 
+    if (item.from_address == props.address.toLowerCase()) {
+      if (isDateRangeSelected && isInDateRange) {
+        gasspentmainnet += (item.fees_paid / 10 ** 18) * item.gas_quote_rate;
+      }
 
-        if (item.from_address != (props.address.toLowerCase())) {
-            // console.log("date", (item.block_signed_at).charAt(8));
-            // console.log("true")
-            // if (props.range[0].include("Jun") && )
-            valuereceivedmainnet += (item.value) / 10 ** 18;
+      if (isDateRangeSelected === false) {
+        gasspentmainnet += (item.fees_paid / 10 ** 18) * item.gas_quote_rate;
+      }
+    }
+  });
 
-        }
+  props.polygondata.data.items.map((item, i) => {
+    let isInDateRange = moment(item.block_signed_at).isBetween(
+      moment(date1).format("YYYY-MM-DD"),
+      moment(date2).format("YYYY-MM-DD")
+    );
+    if (item.from_address != props.address.toLowerCase()) {
+      if (isDateRangeSelected && isInDateRange) {
+        valuereceivedpolygon += item.value / 10 ** 18;
+      }
 
-        if (item.from_address == (props.address.toLowerCase())) {
-            gasspentmainnet += (((item.fees_paid) / 10 ** 18) * item.gas_quote_rate);
-        }
+      if (isDateRangeSelected === false) {
+        valuereceivedpolygon += item.value / 10 ** 18;
+      }
+    }
 
-    })
+    if (item.from_address == props.address.toLowerCase()) {
+      if (isDateRangeSelected && isInDateRange) {
+        gasspentpolygon += (item.fees_paid / 10 ** 18) * item.gas_quote_rate;
+      }
 
-    props.polygondata.data.items.map((item, i) => {
+      if (isDateRangeSelected === false) {
+        gasspentpolygon += (item.fees_paid / 10 ** 18) * item.gas_quote_rate;
+      }
+    }
+  });
 
-        if (item.from_address != (props.address.toLowerCase())) {
+  props.optimismdata.data.items.map((item, i) => {
+    let isInDateRange = moment(item.block_signed_at).isBetween(
+      moment(date1).format("YYYY-MM-DD"),
+      moment(date2).format("YYYY-MM-DD")
+    );
+    if (item.from_address != props.address.toLowerCase()) {
+      if (isDateRangeSelected && isInDateRange) {
+        valuereceivedoptimism += item.value / 10 ** 18;
+      }
 
-            console.log("true")
-            console.log(item.from_address, props.address.toLowerCase());
-            valuereceivedpolygon += (item.value) / 10 ** 18;
+      if (isDateRangeSelected === false) {
+        valuereceivedoptimism += item.value / 10 ** 18;
+      }
+    }
 
-        }
+    if (item.from_address == props.address.toLowerCase()) {
+      if (isDateRangeSelected && isInDateRange) {
+        gasspentoptimism += (item.fees_paid / 10 ** 18) * item.gas_quote_rate;
+      }
 
-        if (item.from_address == (props.address.toLowerCase())) {
-            gasspentpolygon += (((item.fees_paid) / 10 ** 18) * item.gas_quote_rate);
-        }
+      if (isDateRangeSelected === false) {
+        gasspentoptimism += (item.fees_paid / 10 ** 18) * item.gas_quote_rate;
+      }
+    }
+  });
 
-    })
+  return (
+    <>
+      <div>
+        <div style={{ border: "2px solid black" }}>
+          <h2>Mainnet</h2>
+          <h3>Receipts from EOAs: {Number(valuereceivedmainnet)} ETH</h3>
+          <h3>Gas Fees paid till Date: ${Number(gasspentmainnet)}</h3>
+        </div>
 
-    props.optimismdata.data.items.map((item, i) => {
-
-        if (item.from_address != (props.address.toLowerCase())) {
-
-            console.log("true")
-            console.log(item.from_address, props.address.toLowerCase());
-            valuereceivedoptimism += (item.value) / 10 ** 18;
-
-        }
-
-        if (item.from_address == (props.address.toLowerCase())) {
-            gasspentoptimism += (((item.fees_paid) / 10 ** 18) * item.gas_quote_rate);
-        }
-
-    })
-
-
-
-
-    return (
-        <>
-            <div>
-                <div style={{ border: "2px solid black" }}>
-
-                    <h2>Mainnet</h2>
-                    <h3>Receipts from EOAs:  {Number(valuereceivedmainnet)} ETH</h3>
-                    <h3>Gas Fees paid till Date: ${Number(gasspentmainnet)}</h3>
-                </div>
-
-                <div style={{ border: "2px solid black" }}>
-
-                    <h2>Polygon</h2>
-                    <h3>Receipts from EOAs:  {Number(valuereceivedpolygon)} ETH</h3>
-                    <h3>Gas Fees paid till Date: ${Number(gasspentpolygon)}</h3>
-                </div>
-                <div style={{ border: "2px solid black" }}>
-
-                    <h2>Optimism</h2>
-                    <h3>Receipts from EOAs:  {Number(valuereceivedoptimism)} ETH</h3>
-                    <h3>Gas Fees paid till Date: ${Number(gasspentoptimism)}</h3>
-                </div>
-
-
-            </div>
-
-        </>
-    )
+        <div style={{ border: "2px solid black" }}>
+          <h2>Polygon</h2>
+          <h3>Receipts from EOAs: {Number(valuereceivedpolygon)} ETH</h3>
+          <h3>Gas Fees paid till Date: ${Number(gasspentpolygon)}</h3>
+        </div>
+        <div style={{ border: "2px solid black" }}>
+          <h2>Optimism</h2>
+          <h3>Receipts from EOAs: {Number(valuereceivedoptimism)} ETH</h3>
+          <h3>Gas Fees paid till Date: ${Number(gasspentoptimism)}</h3>
+        </div>
+      </div>
+    </>
+  );
 }
-
-
--- pages/GasGuzzler.jsx
+++ pages/GasGuzzler.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
+
 import Gasdata from "../components/Gasdata";
 import API from "../utils/API";
 import { COVALENT_KEY } from "../constants";
 import DateRangePicker from "@wojtekmaj/react-daterange-picker/dist/entry.nostyle";
 import "@wojtekmaj/react-daterange-picker/dist/DateRangePicker.css";
 import "react-calendar/dist/Calendar.css";
+import { useStore } from "../store/useStore";
+
 export default function GasCalculator() {
   const [address, setaddress] = useState("");
   const [mainnetdata, setmainnetdata] = useState(null);
@@ -12,25 +15,28 @@ export default function GasCalculator() {
   const [optimismdata, setoptimismdata] = useState(null);
   const [isLoading, setLoading] = useState(false);
   const [nooftx, setnooftx] = useState(null);
-  const [value, onChange] = useState([new Date(), new Date()]);
-  console.log("onchange", value);
-  const getdata = async (client) => {
+  const [dateRange, setDateRange] = useState([false, false]);
+
+  const [state, dispatch] = useStore();
+
+  const getdata = async (address) => {
     setLoading(true);
     let responsemainnet = await API.get(
       `https://api.covalenthq.com/v1/1/address/` +
-        `${client}` +
+        `${address}` +
         `/transactions_v2/?quote-currency=USD&format=JSON&block-signed-at-asc=false&no-logs=true&key=${COVALENT_KEY}`
     );
     console.log("responesemainnet", responsemainnet);
 
     let itemsdatamainnet = responsemainnet.data;
+    // console.log("itemsdatamainnet: ", itemsdatamainnet);
     setmainnetdata(itemsdatamainnet);
     setLoading(false);
     setnooftx(mainnetdata?.length);
 
     let responsepolygon = await API.get(
       `https://api.covalenthq.com/v1/137/address/` +
-        `${client}` +
+        `${address}` +
         `/transactions_v2/?quote-currency=USD&format=JSON&block-signed-at-asc=false&no-logs=true&key=${COVALENT_KEY}`
     );
 
@@ -41,7 +47,7 @@ export default function GasCalculator() {
 
     let responseoptimism = await API.get(
       `https://api.covalenthq.com/v1/10/address/` +
-        `${client}` +
+        `${address}` +
         `/transactions_v2/?quote-currency=USD&format=JSON&block-signed-at-asc=false&no-logs=true&key=${COVALENT_KEY}`
     );
 
@@ -51,17 +57,18 @@ export default function GasCalculator() {
     setnooftx(optimismdata?.length);
   };
 
+  let accountAddress = state.searchedAddress ? state.searchedAddress : state.address;
   return (
     <>
-      <h1 className="text-3xl font-medium rounded-md bg-violet-300 px-6 py-2 ">Gas Guzzler</h1>
-      <input
+      {/* <h1 className="text-3xl font-medium rounded-md bg-violet-300 px-6 py-2 ">Gas Guzzler</h1> */}
+      {/* <input
         className="border-black-300 margin-10 px-4 py-2 rounded-md m-10"
         placeholder="Enter your address"
         value={address}
         onChange={(e) => setaddress(e.target.value)}
-      />
+      /> */}
 
-      <button
+      {/* <button
         className="rounded-none bg-violet-400 hover:bg-sky-400 px-5 text-2xl "
         onClick={() => {
           console.log(address);
@@ -71,7 +78,12 @@ export default function GasCalculator() {
       </button>
 
       <div>
-        <DateRangePicker format="y-m-dd" onChange={onChange} value={value} />
+        <DateRangePicker
+          onChange={(dateRange) => {
+            setDateRange(dateRange);
+          }}
+          value={dateRange}
+        />
       </div>
 
       <button
@@ -80,22 +92,45 @@ export default function GasCalculator() {
           getdata(address);
         }}>
         Gas Data for this range
-      </button>
-      {isLoading ? <p>Loading...</p> : null}
+      </button> */}
+
+      {/* header actions */}
+      <div className="divider- text-center opacity-90 text-2xl">Gas Guzzler</div>
+      <div className="flex  justify-start items-center my-2">
+        <div className="flex flex-col">
+          <span className="opacity-70">calculate gas in date range</span>
+          <DateRangePicker
+            onChange={(dateRange) => {
+              setDateRange(dateRange);
+            }}
+            value={dateRange}
+            onCalendarClose={() => {
+              getdata(accountAddress);
+            }}
+          />
+        </div>
+        <button className="btn btn-outline btn-primary btn-sm self-end mx-2" onClick={() => getdata(accountAddress)}>
+          Overall gas paid
+        </button>
+      </div>
 
       {!isLoading && mainnetdata && polygondata && optimismdata && (
         <div>
-          {
-            <Gasdata
-              polygondata={polygondata}
-              mainnetdata={mainnetdata}
-              optimismdata={optimismdata}
-              address={address}
-              range={value}
-            />
-          }
+          <Gasdata
+            polygondata={polygondata}
+            mainnetdata={mainnetdata}
+            optimismdata={optimismdata}
+            address={address}
+            range={dateRange}
+          />
         </div>
       )}
+
+      {isLoading ? (
+        <div className="flex justify-center mt-16">
+          <progress className="progress w-52 progress-primary "></progress>
+        </div>
+      ) : null}
     </>
   );
 }
