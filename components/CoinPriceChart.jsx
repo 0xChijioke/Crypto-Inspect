a9738dda2e3e83dc45231ab320e2e16e38cf1b4d
NaimBijapure <naim@vinnovatetechnologies.com>
2022-06-29 10:24:01 +0530

    Title: hotfix: update charte theme on change

--- components/CoinPriceChart.jsx
+++ components/CoinPriceChart.jsx
@@ -120,7 +120,7 @@ const CoinPriceChart = () => {
       chartRef.current = null;
       setCount(!count);
     };
-  }, [tokenPrice, count]);
+  }, [tokenPrice, count, theme]);
 
   const btn =
     "active:bg-gradient-to-r active:from-purple-500 active:via-purple-600 active:to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2";
